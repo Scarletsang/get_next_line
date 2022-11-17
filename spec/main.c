7cd3dedcf8e45143d0cd37e153bf5a884a9f5002
Anthony Tsang <anthonytsang0226@gmail.com>
2022-11-17 05:00:14 +0100

    updated test cases for fixing bugs

--- spec/main.c
+++ spec/main.c
@@ -6,7 +6,7 @@
 /*   By: htsang <htsang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/29 14:52:46 by htsang            #+#    #+#             */
-/*   Updated: 2022/11/16 14:38:33 by htsang           ###   ########.fr       */
+/*   Updated: 2022/11/17 02:54:48 by htsang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,7 @@
 
 int	main(int argc, char **argv)
 {
-	get_n_line(&get_next_line, "inputs/1char.txt", 6);
-	get_n_line(&get_next_line, "inputs/input.txt", 6);
+	get_n_line(&get_next_line, argv[1], atoi(argv[2]));
 	// test2(argv[1]);
 	return (0);
 }
