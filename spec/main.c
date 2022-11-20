40dea668a5ac16cf5d9801caaf03dadbc42519f5
Hei yiu Tsang <htsang@2-I-1.42heilbronn.de>
2022-11-20 21:38:06 +0100

    added test files

--- spec/main.c
+++ spec/main.c
@@ -6,7 +6,7 @@
 /*   By: htsang <htsang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/29 14:52:46 by htsang            #+#    #+#             */
-/*   Updated: 2022/11/17 02:54:48 by htsang           ###   ########.fr       */
+/*   Updated: 2022/11/20 18:20:00 by htsang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 int	main(int argc, char **argv)
 {
+	// return (printf("%zd\n", read(1, NULL, 3)));
 	get_n_line(&get_next_line, argv[1], atoi(argv[2]));
 	// test2(argv[1]);
 	return (0);
