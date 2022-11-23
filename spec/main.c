eecbd6e567557ba5852ab3a4b84a5e030fc500d9
Hei yiu Tsang <htsang@1-C-10.42heilbronn.de>
2022-11-23 23:18:14 +0100

    updated tests

--- spec/main.c
+++ spec/main.c
@@ -6,7 +6,7 @@
 /*   By: htsang <htsang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/29 14:52:46 by htsang            #+#    #+#             */
-/*   Updated: 2022/11/20 18:20:00 by htsang           ###   ########.fr       */
+/*   Updated: 2022/11/23 23:13:28 by htsang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,10 @@
 
 int	main(int argc, char **argv)
 {
-	// return (printf("%zd\n", read(1, NULL, 3)));
-	get_n_line(&get_next_line, argv[1], atoi(argv[2]));
-	// test2(argv[1]);
+	bonus(argv);
+	// get_n_line(&get_next_line, argv[1], atoi(argv[2]));
+	// get_n_line_fd(&get_next_line, STDIN_FILENO, 5);
+	// read_until_end(argv[1]);
+
 	return (0);
 }
--- spec/tests/test.c
+++ spec/tests/test.c
@@ -6,11 +6,15 @@
 /*   By: htsang <htsang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/13 22:27:00 by htsang            #+#    #+#             */
-/*   Updated: 2022/11/16 14:39:59 by htsang           ###   ########.fr       */
+/*   Updated: 2022/11/23 23:16:28 by htsang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "tests.h"
+
+/////////////////////////////////////////
+////////////     input fd     ///////////
+/////////////////////////////////////////
 
 void	get_n_line_fd(test_func func, int fd, size_t n)
 {
@@ -27,10 +31,62 @@ void	get_n_line_fd(test_func func, int fd, size_t n)
 	close(fd);
 }
 
+void	print_get_next_line(int fd)
+{
+	char *str;
+	
+	str = get_next_line(fd);
+	printf("return: [%s]", str);
+	free(str);
+}
+
+////////////////////////////////////////
+//////////   input file_name   /////////
+////////////////////////////////////////
+
 void	get_n_line(test_func func, const char *file_name, size_t n)
 {
 	int fd;
 
 	fd = open(file_name,  O_RDONLY);
 	get_n_line_fd(func, fd, n);
+}
+
+void	read_until_end(char *file_name)
+{
+	char	*str;
+	int		fd;
+
+	fd = open(file_name, O_RDONLY);
+	str = get_next_line(fd);
+	while (str)
+	{
+		printf("[%s]", str);
+		free(str);
+		str = get_next_line(fd);
+	}
+	close(fd);
+}
+
+//////////////////////////////////////////
+////////////       bonus       ///////////
+//////////////////////////////////////////
+
+void	bonus(char **argv)
+{
+	int i;
+	int	fd1 = open(argv[1],  O_RDONLY);
+	int	fd2 = open(argv[2],  O_RDONLY);
+	int	fd3 = open(argv[3],  O_RDONLY);
+
+	while (i++ < 6)
+	{
+		print_get_next_line(fd1);
+		print_get_next_line(fd2);
+		print_get_next_line(fd3);
+	}
+
+	close(fd1);
+	close(fd2);
+	close(fd3);
 }
